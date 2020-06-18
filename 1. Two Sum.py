f237a99df9f15f9019312d619420f772b2fbe1ac
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-18 00:10:44 -0700

    add Javascript solution.

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -27,3 +27,22 @@ class Solution:
 #             if value in dict:
 #                 return [dict[value], index]
 #             dict[target - value] = index
+
+# /** Javascript
+#  * @param {number[]} nums
+#  * @param {number} target
+#  * @return {number[]}
+#  */
+# var twoSum = function(nums, target) {
+#     const map = {};
+    
+#     for(let i = 0; i < nums.length; i++) {
+#         if(map[nums[i]] !== undefined) {
+#             return [map[nums[i]], i];
+#         }
+#         else {
+#             map[target - nums[i]] = i;
+#         }
+#     }
+    
+# };
