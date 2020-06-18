99d1e77c19ef930ffe6bbfff0bcdb903b85b2899
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-18 00:11:58 -0700

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, nums, target):
-        n=len(nums)
-        A= sorted(zip(nums, range(n)))
-        left, right=0, n-1
-        while left<right:
-            if A[left][0]+A[right][0]==target:
+        n = len(nums)
+        A = sorted(zip(nums, range(n)))
+        left, right = 0, n-1
+        while left < right:
+            if A[left][0] + A[right][0] == target:
                 return [A[left][1], A[right][1]] if A[left][1]< A[right][1] else [A[right][1], A[left][1]]
-            elif A[left][0]+ A[right][0]<target:
-                left+=1
-            else: right-=1
+            elif A[left][0] + A[right][0] < target:
+                left += 1
+            else: 
+                right -= 1
 
     nums=[7, 2,10,11]
     target=9
@@ -35,7 +36,6 @@ class Solution:
 #  */
 # var twoSum = function(nums, target) {
 #     const map = {};
-    
 #     for(let i = 0; i < nums.length; i++) {
 #         if(map[nums[i]] !== undefined) {
 #             return [map[nums[i]], i];
@@ -44,5 +44,4 @@ class Solution:
 #             map[target - nums[i]] = i;
 #         }
 #     }
-    
 # };
