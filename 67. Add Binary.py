4d5701a1f7de4c52a0a6efa7907ba8d7b0f093ad
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-23 11:02:54 -0700

    Update 67. Add Binary.py

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -8,7 +8,7 @@ class Solution(object):
         ans, m, n, carry = '', len(a) - 1, len(b) - 1, 0
         while m >= 0 or n >= 0:
             x = int(a[m]) if m >= 0 else 0
-            y = int(b[n]) if b >= 0 else 0
+            y = int(b[n]) if n >= 0 else 0
             carry, digit = (x + y + carry) // 2, (x + y + carry) % 2
             if digit:
                 ans = '1' + ans
@@ -17,3 +17,12 @@ class Solution(object):
             m -= 1
             n -= 1
         return ans if carry == 0 else '1' + ans
+
+def execute() -> object:
+    a, b = "11", "1"
+    sol = Solution()
+    print(sol.addBinary(a, b))
+
+if __name__ == "__main__":
+    execute()
+
