0c7b760fb44496f4bfbb6d2c31024b3ba2c486ae
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-23 14:06:31 -0700

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -9,7 +9,9 @@ class Solution(object):
         if not nums or n < 3 or nums[0] > 0 or nums[-1] < 0:
             return []
         for i in range(n - 2):
-            if i and nums[i] == nums[i - 1]:
+            if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+                break
+            if (i and nums[i] == nums[i - 1]) or (nums[i] + nums[n - 2] + nums[n - 1] < 0):
                 continue
             left, right = i + 1, n - 1
             while left < right:
@@ -36,4 +38,48 @@ def execute():
 if __name__ == "__main__":
     execute()
 
+# /** Javascript
+#  * @param {number[]} nums
+#  * @return {number[][]}
+#  */
+# var threeSum = function(nums) {
+#     nums = nums.sort((a, b) => a - b);
+#     let n = nums.length;
+#     if (n < 3 || nums[0] > 0 || nums[n - 1] < 0)
+#             return [];
+#     let ans = [];
+    
+#     for(let i = 0; i < n - 2; i++) {
+#         if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+        
+#         if (i > 0 && nums[i] === nums[i - 1]) continue;
+        
+#         if (nums[i] + nums[n - 2] + nums[n - 1] < 0) continue;
+        
+#         let left = i + 1,
+#             right = n - 1;
+#         while(left < right) {
+#             let target = nums[i] + nums[left] + nums[right];
+#             if(target === 0) {
+#                 ans.push([nums[i], nums[left], nums[right]]);
+#                 left ++;
+#                 right --;
+#                 while(left < right && nums[left] === nums[left - 1]) {
+#                     left ++;
+#                 }
+#                 while(left < right && nums[right] === nums[right + 1]) {
+#                     right --;
+#                 }
+#             }
+#             else if(target < 0) {
+#                 left ++;
+#             }
+#             else {
+#                 right --;
+#             }
+#         }
+#     }
+    
+#     return ans;
+# };
 
