066f13cb2246d40adcea8974f6850f958152f848
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-24 22:45:02 -0700

    Update 3. Longest Substring Without Repeating Characters.py

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -21,3 +21,28 @@ def execute() -> object:
 if __name__ == "__main__":
     execute()
 
+# /** Javascript
+#  * @param {string} s
+#  * @return {number}
+#  */
+# var lengthOfLongestSubstring = function(s) {
+#     if(s === '' || s.length === 0) return 0;
+    
+    
+#     let start = 0;
+#     let map = {};
+#     let ans = 0;
+    
+#     for(let i = 0; i < s.length; i++) {
+#         let ch = s[i];
+#         // check if the char in the map, update the index position 
+#         if(ch in map && map[ch] >= start) {
+#             start = map[ch] + 1;
+#         }
+#         else {
+#             ans = Math.max(ans, i - start + 1);
+#         }
+#         map[ch] = i;
+#     }
+#     return ans;
+# };
