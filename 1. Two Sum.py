4447c512719de747682a552d48fb3e70477581d6
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-06-26 23:50:34 -0700

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -29,6 +29,30 @@ class Solution:
 #                 return [dict[value], index]
 #             dict[target - value] = index
 
+# R solution to Leetcode.
+# twoSum = function(nums, target){
+#   n = length(nums)
+#   a = cbind(nums, 1:n)
+#   A = as.matrix(a[order(a[,1]), ])
+#   left = 1
+#   right = n
+#   while (left < right){
+#     if (A[left, 1] + A[right, 1] == target){
+#         ans = c(A[left, 2], A[right, 2])  
+#         break
+#     }
+#     else if (A[left, 1] + A[right, 1] < target){
+#       left =  left + 1
+#     }
+#     else {
+#       right = right - 1
+#     }
+#   }
+#    sort(ans)
+# }
+
+# twoSum(c(7, 2, 10, 11), 9)  
+
 # /** Javascript
 #  * @param {number[]} nums
 #  * @param {number} target
