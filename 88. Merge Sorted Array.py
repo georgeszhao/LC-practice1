9aed5c4941bf6977843217ad3a984561a838a4cf
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-07-08 01:00:34 -0700

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -30,4 +30,27 @@ def execute() -> object:
 
 if __name__ == "__main__":
     execute()
-
+    
+# # Javascript
+# var merge = function(nums1, m, nums2, n) {
+#     let i = m - 1;  
+#     let j = n - 1;
+#     let k = m + n - 1;
+#     while(i >= 0 && j >= 0) {
+#         if(nums2[j] >= nums1[i]) {
+#             nums1[k] = nums2[j];
+#             j--;
+#             k--;
+#         } else {
+#             nums1[k] = nums1[i];
+#             i--;
+#             k--;
+#         }
+#     } 
+#     // j < 0, nums1's looping is over, then continue nums2
+#      while(j >= 0) {
+#         nums1[k] = nums2[j];
+#         j--;
+#         k--;
+#     }
+# };
