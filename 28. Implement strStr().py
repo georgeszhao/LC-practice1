254a9e8a6081cd3a4ed3ed1b6ddf5750ab19fd4b
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-07-11 23:55:38 -0700

    Add files via upload

---
+++ 28. Implement strStr().py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+        m, n = len(haystack), len(needle)
+        for i in range(m - n + 1):
+            if haystack[i] != needle[0]:
+                continue
+            if haystack[i: i + n] == needle:
+                return i
+        return -1
