fdfd62db37b856a8cf980e91a021e3ee0c5db924
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-07-16 12:38:32 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -49,3 +49,38 @@ if __name__ == "__main__":
 #             L -= 1
 #             R += 1
 #         return R - L - 1
+
+# /** Javascript
+#  * @param {string} s
+#  * @return {string}
+#  */
+# var longestPalindrome = function(s) {
+#     if(s === '' || s.length === 0) return '';
+    
+#     let n = s.length,
+#         left = 0,
+#         right = 0,
+#         max_len = 0,
+#         dp = [];
+    
+#     for (let i = 0; i < n; i++) {
+#         dp[i] = [];
+#         for (let j = 0; j < i + 1; j++) {
+#             dp[i][j] = 0;
+#         }
+#     }
+    
+#     for (let i = 0; i < n; i++) {
+#         dp[i][i] = true;
+#         for (let j = 0; j < i; j++) {
+#             dp[i][j] = (s[i] == s[j]) && (i - j == 1 || dp[i - 1][j + 1] == true)
+#                 //(s[i] == s[j] && i - j == 1)  || (s[i] == s[j] && dp[i - 1][j + 1] == true)
+#             if (dp[i][j] == true && i - j + 1 > max_len) {
+#                  left = j,
+#                  right = i,
+#                  max_len = i - j + 1;
+#                 }
+#         }
+#     }
+#     return s.substring(left, right + 1);
+# };
