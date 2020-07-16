03ebc4a079bef806baa8ca6ef9f24414e402acc7
georgeszhao <54526640+georgeszhao@users.noreply.github.com>
2020-07-16 13:27:15 -0700

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -50,6 +50,35 @@ if __name__ == "__main__":
 #             R += 1
 #         return R - L - 1
 
+# # R for Leetcode
+# longestPalindrome <- function(s) {
+#   if (nchar(s) < 1) {
+#     return('')
+#   }
+#   left = 0
+#   right = 0
+#   max_len = 0
+#   n = nchar(s)
+#   dp = matrix(rep(0, n * n), nrow = n)
+#   for (i in 1: n) {
+#     dp[i, i] = 1
+#     j = 1
+#     while (j < i) {
+#       if (substr(s, i, i) == substr(s, j, j) && (i - j == 1 || dp[i - 1, j + 1] == 1)) {
+#         dp[i, j] = 1
+#       } 
+#       if (dp[i, j] == 1 && max_len < i -j + 1) {
+#         left = j
+#         right = i
+#         max_len = i - j + 1
+#       }
+#       j = j + 1
+#     }
+#   }
+#   substr(s, left, right)
+# }
+# longestPalindrome('babad')
+
 # /** Javascript
 #  * @param {string} s
 #  * @return {string}
